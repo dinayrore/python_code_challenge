496e8899bd70b71dc02221878dd71a7e04563192
Kristine <kteich88@gmail.com>
2017-02-22 16:13:46 -0500

    Added code to render index in views.py.

--- codechallenge/appointments/views.py
+++ codechallenge/appointments/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Appointment
+
+def index(request):
+    latest_appointment_list = Appointment.objects.order_by('-date')[:5]
+    context = {'latest_appointment_list': latest_appointment_list}
+    return render(request, 'appointments/index.html', context)
