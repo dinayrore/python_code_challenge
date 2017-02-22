b91c126e216389f39dce17d160e220ae51566277
Kristine <kteich88@gmail.com>
2017-02-22 11:21:08 -0500

    Added appointments class to models.py; Added date, time, and description relationships.

--- codechallenge/appointments/models.py
+++ codechallenge/appointments/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model)
+    date = models.DateTimeField('date')
+    time = models.DateTimeField('time')
+    description = models.CharField(max_length=200)
