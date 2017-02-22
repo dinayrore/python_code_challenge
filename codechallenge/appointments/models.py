a5a2fbe6291a774c4659053e6483a9947f5c6ea6
Kristine <kteich88@gmail.com>
2017-02-22 11:33:52 -0500

    Added missing colon after class name. Fixed syntax error.

--- codechallenge/appointments/models.py
+++ codechallenge/appointments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Appointment(models.Model)
+class Appointment(models.Model):
     date = models.DateTimeField('date')
     time = models.DateTimeField('time')
     description = models.CharField(max_length=200)
