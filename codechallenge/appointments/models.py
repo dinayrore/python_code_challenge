a13a63e25690515e3186c468c1d7cc625e51bb26
Kristine <kteich88@gmail.com>
2017-02-23 15:21:16 -0500

    Updated models to be more specific to data types require.

--- codechallenge/appointments/models.py
+++ codechallenge/appointments/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Appointment(models.Model):
-    date = models.DateTimeField('date')
-    time = models.DateTimeField('time')
+    date = models.DateField('date')
+    time = models.TimeField('time')
     description = models.CharField(max_length=200)
 
     def __str__(self):
