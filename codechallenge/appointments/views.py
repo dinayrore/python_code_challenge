352817d444ae320ceb5ea43197e33e683464a1fe
Kristine <kteich88@gmail.com>
2017-02-23 09:21:25 -0500

    Updating new function for POST method in views.py.

--- codechallenge/appointments/views.py
+++ codechallenge/appointments/views.py
@@ -6,3 +6,21 @@ def index(request):
     latest_appointment_list = Appointment.objects.order_by('-date')[:5]
     context = {'latest_appointment_list': latest_appointment_list}
     return render(request, 'appointments/index.html', context)
+
+def new(request, date, time, description):
+    appointment = get_object_or_404(Appointment, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
