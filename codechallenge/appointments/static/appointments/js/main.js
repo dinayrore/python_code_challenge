55bcc2362dda9b29b56f6fcb695a811b83fe3020
Kristine <kteich88@gmail.com>
2017-02-23 15:22:54 -0500

    Updated main.js to include POST and GET functions.

--- codechallenge/appointments/static/appointments/js/main.js
+++ codechallenge/appointments/static/appointments/js/main.js
@@ -1,107 +1,64 @@
-var label = $(".btn").text();
+// TODO: use double quotes
 
-$(".btn-success").click(function() {
+var apptFormContainer = $('#appt-form-container');
+var apptForm = $('#appt-form');
+var newApptBtn = $('#new-appt');
+var cancelApptBtn = $('#cancel-appt');
+var dateInput = $('#appt-date-input');
+var timeInput = $('#appt-time-input');
+var descriptionInput = $('#appt-description-input');
 
- if(label == "ADD") {
-  //  $("#form").hide();
-  // TODO: POST to DB on success do the same as btn-danger click
-  // Submit post on submit
-  $('#post-form').on('submit', function(event){
-      event.preventDefault();
-      console.log("Your appointment is saved")
-      create_post();
-  });
- }
- else {
-   $(".btn").text("ADD");
-   $("#form").show();
- }
-
+newApptBtn.click(function() {
+  var btnText = $(this).text();
+  if (btnText == "ADD") {
+    apptForm.submit();
+  } else {
+    newApptBtn.text("ADD");
+    apptFormContainer.show();
+  }
 });
 
-$(".btn-danger").click(function() {
-  $(".btn-success").text("NEW");
-  $("#form").hide();
-
+cancelApptBtn.click(function() {
+  newApptBtn.text("NEW");
+  apptFormContainer.hide();
 });
 
-
-
-// Another method for AJAX POST?
 $(document).ready(function() {
-    $("#post").click(function(e) {
-        e.preventDefault();
-        var data = {
-            '': ''
-        }
-
-        $.ajax({
-            "type": "POST",
-            "dataType": "json",
-            "url": "http://127.0.0.1:8000/",
-            "data": data,
-            "success": function(result) {
-                console.log(result);
-            },
-        });
-    });
+  getAppointments();
 });
 
-
-
-
-
-
-// AJAX request from Postman
-var form = new FormData();
-form.append("date", "02/22/17");
-form.append("time", "4:49 PM");
-form.append("description", "Enter Data into sqlite db");
-
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "http://127.0.0.1:8000/",
-  "method": "POST",
-  "headers": {
-    "cache-control": "no-cache",
-    "postman-token": "124baa8a-0703-ec7d-ca71-cedde46d5ceb"
-  },
-  "processData": false,
-  "contentType": false,
-  "mimeType": "multipart/form-data",
-  "data": form
+function getAppointments(searchString) {
+  $.ajax({
+    method: "GET",
+    url: "/getAppointments",
+    dataType: "json"
+  }).then(function(appointments) {
+    console.log(appointments);
+  }).catch(function(err) {
+    console.error(err);
+  })
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-
-
-
-
-
-
-// Something I need to use CSRF?
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    }
-});
+// // Something I need to use CSRF?
+// function getCookie(name) {
+//     var cookieValue = null;
+//     if (document.cookie && document.cookie != '') {
+//         var cookies = document.cookie.split(';');
+//         for (var i = 0; i < cookies.length; i++) {
+//             var cookie = jQuery.trim(cookies[i]);
+//             if (cookie.substring(0, name.length + 1) == (name + '=')) {
+//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                 break;
+//             }
+//         }
+//     }
+//     return cookieValue;
+// }
+//
+// $.ajaxSetup({
+//     beforeSend: function(xhr, settings) {
+//         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+//             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+//         }
+//     }
+// });
