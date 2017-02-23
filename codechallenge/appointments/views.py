17f812e3acee5daa65c5beab771b310147e61347
Kristine <kteich88@gmail.com>
2017-02-23 15:24:02 -0500

    Updated views.py to include POST functionality.

--- codechallenge/appointments/views.py
+++ codechallenge/appointments/views.py
@@ -3,9 +3,20 @@ from django.shortcuts import render
 from .models import Appointment
 
 def index(request):
+    if request.method == 'POST':
+        date = request.POST['appt_date']
+        time = request.POST['appt_time']
+        description = request.POST['appt_description']
+        appt = Appointment(date=date, time=time, description=description)
+        appt.save()
+
+    # if request.method == 'GET':
+        # TODO: and they want json data from ajax, return json
+
     latest_appointment_list = Appointment.objects.order_by('-date')[:5]
     context = {'latest_appointment_list': latest_appointment_list}
     return render(request, 'appointments/index.html', context)
 
 def post(self, request):
-        return HttpResponse(json.dumps({'key': 'value'}), mimetype="application/json")
+    print("this is a post, pretty sure this never happens")
+    return HttpResponse(json.dumps({'key': 'value'}), mimetype="application/json")
