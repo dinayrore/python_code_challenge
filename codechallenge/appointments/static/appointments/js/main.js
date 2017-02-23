f74e60342d5b88883aeb56944d2ddfe3709672b7
Kristine <kteich88@gmail.com>
2017-02-23 09:19:45 -0500

    Added POST function to main.js.

--- codechallenge/appointments/static/appointments/js/main.js
+++ codechallenge/appointments/static/appointments/js/main.js
@@ -5,6 +5,12 @@ $(".btn-success").click(function() {
  if(label == "ADD") {
   //  $("#form").hide();
   // TODO: POST to DB on success do the same as btn-danger click
+  // Submit post on submit
+  $('#post-form').on('submit', function(event){
+      event.preventDefault();
+      console.log("Your appointment is saved")  // sanity check
+      create_post();
+  });
  }
  else {
    $(".btn").text("ADD");
